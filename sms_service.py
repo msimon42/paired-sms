4e12cae4e9cf429bfaff17c0d4d5b6ca580d73ed
msimon42 <msimon@uab.edu>
2020-02-19 17:52:21 -0700

    Add methods to request and confirm appointments

--- sms_service.py
+++ sms_service.py
@@ -7,9 +7,18 @@ class SmsService:
         self.auth_token = os.environ['TWILIO_KEY']
         self.client = Client(self.account_sid, self.auth_token)
 
-    def appointment_confirmation(self, phone_number):
+    def appointment_request(self, phone_number, name, time):
         message = self.client.messages.create(
-                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     body=f"Hello! {name} has requested a pairing at {time}. Visit https://paired-turing.firebaseapp.com/schedule to confirm",
+                     from_='+17147331519',
+                     to=phone_number
+                 )
+
+        return message.sid
+
+    def appointment_confirmation(self, phone_number, name, time):
+        message = self.client.messages.create(
+                     body=f"Hello! {name} has confirmed your request to pair at {time}. You're all set!",
                      from_='+17147331519',
                      to=phone_number
                  )
