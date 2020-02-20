03ce93dea8461546769564ee6c98c900ffc47fe8
msimon42 <msimon@uab.edu>
2020-02-19 21:43:59 -0700

    Refactor

--- app.py
+++ app.py
@@ -4,24 +4,20 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from sms_service import SmsService
 
-def create_app():
-    app = Flask(__name__)
+
+app = Flask(__name__)
 
 
-    @app.route("/request", methods=['POST'])
-    def request_notif():
-        data = request.get_json(force=True)
-        if data['sms_token'] == os.environ['SMS_TOKEN']:
-            message = SmsService()
-            return message.appointment_request(data['phone_number'], data['name'], data['time'])
-        else:
-            return 'Invalid token. Message not sent.'
+@app.route("/request", methods=['POST'])
+def request_notif():
+    data = request.get_json(force=True)
+    if data['sms_token'] == os.environ['SMS_TOKEN']:
+        message = SmsService()
+        return message.appointment_request(data['phone_number'], data['name'], data['time'])
+    else:
+        return 'Invalid token. Message not sent.'
 
 
-    return app
-
-
-app = create_app()
 
 if __name__ == "__main__":
     app.run()
