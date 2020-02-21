b8834d13f7e93155aebdcf7201eb6ef069db0ef0
msimon42 <msimon@uab.edu>
2020-02-20 17:57:06 -0700

    Add method to check valid request

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sms_service
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from sms_service import SmsService
-from helpers import convert_to_international
+from helpers import convert_to_international, generate_response, check_valid_request
 
 app = Flask(__name__)
 CORS(app)
@@ -11,11 +11,11 @@ CORS(app)
 @app.route("/request", methods=['POST'])
 def request_notif():
     data = request.get_json(force=True)
-    if data['sms_token'] == os.environ['SMS_TOKEN']:
+    if check_valid_request(data):
         message = SmsService()
         return message.message_request(convert_to_international(data['phone_number']), data['message'])
     else:
-        return 'Invalid token. Message not sent.'
+        return generate_response('Invalid request')
 
 
 
